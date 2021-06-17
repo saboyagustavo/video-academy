cde83dad44691855c276336ea799363cbd7b841a
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-06-17 14:28:31 -0300

    Configured API connection.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 function App() {
   return (
-    <div className="container">
+    <div className="center">
       <h1>Video Academy</h1>
     </div>
   );
